f94407881dd2f74e63ec88deb28c0ef4e5829f89
Andre Andre Andre <andredupdm@gmail.com>
2023-06-11 15:22:36 -0300

    feat: add basic api to run node serve

---
+++ main.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Your NodeServer');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
